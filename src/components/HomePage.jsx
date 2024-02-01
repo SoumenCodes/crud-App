ece46c69dff11f3ba53d856dc550d4d9428ea44d
SoumenCodes <soumen19j@gmail.com>
2024-02-02 01:38:44 +0530

    tailwind stuff & Fetch products in HomePage component

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import { setProducts } from "../slice/productSlice";
 
-function HomePage() {
-  return <div>HomePage</div>;
-}
+const HomePage = () => {
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.products);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/products")
+      .then((response) => dispatch(setProducts(response.data)))
+      .catch((error) => console.error(error));
+  }, [dispatch]);
+
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>Product List</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.name} />
+            <p>{product.name}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default HomePage;
